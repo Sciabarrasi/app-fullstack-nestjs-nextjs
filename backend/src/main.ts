ada8c74e3f5ff82f902696c4d47b16e56b62066a
Sciabarrasi <gianluca_sciabarrasi@hotmail.com>
2024-10-03 03:37:22 -0300

    Completed the backend implementation, need to start the frontend

---
+++ backend/src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+  .setTitle('Products example')
+  .setDescription('Products API')
+  .setVersion('1.0')
+  .addTag('Products')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
+
+  await app.listen(4000);
+}
+
+bootstrap();
